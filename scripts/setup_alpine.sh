1f28f5d262cb14099ec0fe46866be4b88985e801
Sam Briesemeister <sam.briesemeister@gmail.com>
2018-05-30 22:23:31 -0700

    Pre-install several Python dependencies

--- scripts/setup_alpine.sh
+++ scripts/setup_alpine.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 set -euf -o pipefail
+set -x
 
 DEV_PACKAGES="libffi-dev libjpeg-turbo-dev libressl-dev python2-dev musl-dev zlib-dev"
-BUILD_PACKAGES="gcc"
+BUILD_PACKAGES="gcc build-base"
+PYTHON_ALPINE="py2-cffi py2-openssl py2-pip python2"
 
-apk --no-cache add ${DEV_PACKAGES} ${BUILD_PACKAGES}  python2 py2-pip zlib make wget libressl
+apk --no-cache add ${DEV_PACKAGES} ${BUILD_PACKAGES} ${PYTHON_ALPINE} zlib wget libressl 
 
 # Python dependencies
 pip install -q unittest2 pytest nose flake8 virtualenv
-pip install -q Pillow 
+pip install -q Pillow
+pip install -q 'urllib3[secure]'
 
 # NB: applications of this container must install more Python packages, which can require
 # compiled modules, i.e. requiring gcc and friends to be present in the container.
