127d34b2bccf8e2ec64cb9cd0c0618df5aa2fe08
Sam Briesemeister <sam.briesemeister@gmail.com>
2018-05-30 18:02:46 -0700

    Several Python testing & development tools

--- scripts/setup_alpine.sh
+++ scripts/setup_alpine.sh
@@ -8,7 +8,7 @@ BUILD_PACKAGES="gcc"
 apk --no-cache add ${DEV_PACKAGES} ${BUILD_PACKAGES}  python2 py2-pip zlib make wget libressl
 
 # Python dependencies
-pip install -q unittest2
+pip install -q unittest2 pytest nose flake8 virtualenv
 pip install -q Pillow 
 
 apk del -r ${DEV_PACKAGES} ${BUILD_PACKAGES} 
